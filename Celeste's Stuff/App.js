321984edda5f9707968149c18eacf2a7f91f697e
Linx2022 <101667371+Linx2022@users.noreply.github.com>
2022-11-03 15:10:22 -0500

    Update App.js

--- Celeste's Stuff/App.js
+++ Celeste's Stuff/App.js
@@ -1,25 +1,106 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
+//import Histogram from "react-chart-histogram";
+import {
+    Chart as ChartJS,
+    CategoryScale,
+    LinearScale,
+    BarElement,
+    Title,
+    Tooltip,
+    Legend,
+  } 
+
+from 'chart.js';
+import { Bar } from 'react-chartjs-2';
+  
+  ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    BarElement,
+    Title,
+    Tooltip,
+    Legend
+  );
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [tomatoes, setTomatoes] = useState([]);
+    const labels = ['Tomatoes'];
+    const options = {
+        responsive: true,
+        plugins: {
+          legend: {
+            position: 'top',
+          },
+          title: {
+            display: true,
+            text: 'Tomatoes by Count',
+          },
+        },
+      };
+    
+    const data = {
+    labels,
+    datasets: [
+        {
+        label: 'Green',
+        data: [(tomatoes.filter(tomato => tomato.Stage === "Green")).length],
+        backgroundColor: 'rgba(0, 160, 0, 5)',
+        },
+        {
+        label: 'Breaker',
+        data: [(tomatoes.filter(tomato => tomato.Stage === "Breaker")).length],
+        backgroundColor: 'rgba(153, 204, 0, 5)',
+        },
+        {
+        label: 'Turning',
+        data: [(tomatoes.filter(tomato => tomato.Stage === "Turning")).length],
+        backgroundColor: 'rgba(255, 179, 102, 5)',
+        },
+        {
+        label: 'Pink',
+        data: [(tomatoes.filter(tomato => tomato.Stage === "Pink")).length],
+        backgroundColor: 'rgba(255, 128, 128, 5)',
+        },
+        {
+        label: 'Light Red',
+        data: [(tomatoes.filter(tomato => tomato.Stage === "Light Red")).length],
+        backgroundColor: 'rgba(255, 77, 77, 5)',
+        },
+        {
+        label: 'Red',
+        data: [(tomatoes.filter(tomato => tomato.Stage === "Red")).length],
+        backgroundColor: 'rgba(230, 57, 0, 5)',
+        },
+        {
+        label: 'Deffective',
+        data: [(tomatoes.filter(tomato => tomato.Stage === "Deffective")).length],
+        backgroundColor: 'rgba(77, 0, 0, 5)',
+        },
+    ],
+    };
+
+    //const options = {fillColor: '#F93208', strokeColor: '#F93208'};
+    const fetchData = () => {
+        fetch("https://cnpu0bqb4i.execute-api.us-east-1.amazonaws.com/beta/items")
+            .then(response => {
+                return response.json()
+            })
+            .then(data => {
+                setTomatoes(data.Items)
+            })
+    }
+
+
+    useEffect(() => {
+        fetchData()
+      }, [])
+
+      return (
+        <>
+            <Bar options={options} data={data} />
+        </>
+    )
 }
 
 export default App;
