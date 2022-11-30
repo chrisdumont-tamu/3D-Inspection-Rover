4cf814c289e90b103875d6201ddf7b9b994c61b3
Linx2022 <101667371+Linx2022@users.noreply.github.com>
2022-11-29 19:25:11 -0600

    Update index.js

--- Celeste's Stuff/index.js
+++ Celeste's Stuff/index.js
@@ -1,11 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+//intialize of the react which is coming from react
+import React from 'react'; //entry point to the react library
+import ReactDOM from 'react-dom/client'; //package provides client-specific methods used for initializing an app on the client
+import './index.css'; //imports the css file into the JavaScript file
+import App from './App'; //generates App.js
+import reportWebVitals from './reportWebVitals'; //measures the performance of the app
+//configure the project to be used with AWS
+import * as AWS from 'aws-sdk';
+//import DWChart from "react-datawrapper-chart";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+
+//Now I set the AWS region for the JS code
+const configuration = {
+  region: 'us-east-1',
+  secretAccessKey: 't/k7TDOqf4vSD3I8gR6AOgyDTp1R56ArWSF0A6AY',
+  accessKeyId: 'AKIAWZ2XQVV2TWHSYQE7',
+}
+
+AWS.config.update(configuration); //set the global region setting
+
+
+const root = ReactDOM.createRoot(document.getElementById('root')); //render react element, passes the DOM element
+root.render( //pass react element
   <React.StrictMode>
     <App />
   </React.StrictMode>
@@ -14,4 +29,5 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals(); //gives the performance of app
+
